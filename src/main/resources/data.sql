dc5939f370436b839ee0e02c7e1fee90c64c4559
Stephen Stage <stagestephen1@gmail.com>
2024-10-01 21:28:43 -0500

    assignment seeding

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,3 +13,22 @@ values
         'Second Thread',
         'This is the second thread'
     );
+
+    --assignment seeding
+    insert into
+    assignments(name, score, due_date)
+values
+    (
+        'HW1',
+        '0/100',
+        '2024-10-14 23:59:59'
+    );
+
+insert into
+    assignments(name, score, due_date)
+values
+    (
+        'HW2',
+        '0/100',
+        '2024-10-21 23:59:59'
+    );
