477b460c7182f0a3c2435d1d5f03cd6945da89a0
Kris Pento <kpento@depaul.edu>
2024-10-21 21:45:53 -0400

    Adding Service/Controller class for Student

---
+++ src/test/java/edu/depaul/cdm/se452/d2l_mock/student/StudentServiceTest.java
@@ -0,0 +1,35 @@
+package edu.depaul.cdm.se452.d2l_mock.student;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class StudentServiceTest {
+    @Autowired
+    private StudentRepository repo;
+
+    @Autowired
+    private MockMvc mvc;
+
+    private static final String url = "/api/students";
+
+    @Test
+    public void getStudents() throws Exception{
+        ResultActions response = mvc.perform(MockMvcRequestBuilders.get(url));
+
+        int count = (int)repo.count();
+
+        response.andExpect(MockMvcResultMatchers.status().isOk());
+		response.andExpect(MockMvcResultMatchers.jsonPath("$.size()", CoreMatchers.is(count)));
+
+    }
+    
+}
