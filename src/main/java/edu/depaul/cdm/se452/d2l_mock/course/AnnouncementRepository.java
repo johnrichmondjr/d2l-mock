b9855c6d32bec638586c9b42325a454e1151fe5d
Dylan Neal <dyl.neal@gmail.com>
2024-10-23 10:23:35 -0500

    Added CourseService
    Encapsulates business logic for Course

--- src/main/java/edu/depaul/cdm/se452/d2l_mock/course/AnnouncementRepository.java
+++ src/main/java/edu/depaul/cdm/se452/d2l_mock/course/AnnouncementRepository.java
@@ -1,5 +1,6 @@
 package edu.depaul.cdm.se452.d2l_mock.course;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
