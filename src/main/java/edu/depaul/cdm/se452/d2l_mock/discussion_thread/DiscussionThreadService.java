6af4327be4dfeff283cabb8e2946525147dd045f
John Richmond <jrichmond@enova.com>
2024-10-24 21:54:28 -0500

    adding logging for thread and post services

--- src/main/java/edu/depaul/cdm/se452/d2l_mock/discussion_thread/DiscussionThreadService.java
+++ src/main/java/edu/depaul/cdm/se452/d2l_mock/discussion_thread/DiscussionThreadService.java
@@ -3,17 +3,25 @@ package edu.depaul.cdm.se452.d2l_mock.discussion_thread;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import lombok.extern.log4j.Log4j2;
 
 @Service
+@Log4j2
 public class DiscussionThreadService {
     @Autowired
     private DiscussionThreadRepository repo;
 
     public List<DiscussionThread> list() {
-        return repo.findAll();
+        log.traceEntry("Fetching List of Discussion Threads");
+        var retval = repo.findAll();
+        log.traceExit("Done fetching list of Discussion Threads", retval);
+        return retval;
     }
 
     public DiscussionThread save(DiscussionThread discussionThread) {
-        return repo.save(discussionThread);
+        log.traceEntry("Preparing to save Discussion Thread");
+        repo.save(discussionThread);
+        log.traceExit("Done saving Discussion Thread", discussionThread);
+        return discussionThread;
     }
 }
--- src/main/java/edu/depaul/cdm/se452/d2l_mock/discussion_thread/PostService.java
+++ src/main/java/edu/depaul/cdm/se452/d2l_mock/discussion_thread/PostService.java
@@ -3,17 +3,25 @@ package edu.depaul.cdm.se452.d2l_mock.discussion_thread;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import lombok.extern.log4j.Log4j2;
 
 @Service
+@Log4j2
 public class PostService {
     @Autowired
     private PostRepository repo;
 
     public List<Post> list() {
-        return repo.findAll();
+        log.traceEntry("Fetching List of Posts");
+        var retval = repo.findAll();
+        log.traceExit("Done fetching list of Posts", retval);
+        return retval;
     }
 
     public Post save(Post post) {
-        return repo.save(post);
+        log.traceEntry("Preparing to save Post");
+        repo.save(post);
+        log.traceExit("Done saving Post", post);
+        return post;
     }
 }
