ebfc0c4a4d8f53943536e84ffe7ab0039874cfb2
John Richmond <jrichmond@enova.com>
2024-10-30 16:16:05 -0500

    updating package info java to include test info

--- src/main/java/edu/depaul/cdm/se452/d2l_mock/discussion_thread/package-info.java
+++ src/main/java/edu/depaul/cdm/se452/d2l_mock/discussion_thread/package-info.java
@@ -9,13 +9,13 @@
  * <b>This package contains the classes that are used to create a discussion
  * thread
  * <ul>
- * <li>Unit Tests
  * <li>UI for listing Discussion Threads and creating new Discussion
  * Threads
  * <li>UI for showing a single Discussion Thread and adding a Post to a
  * Discussion Thread
- * <li>Added unit tests to demonstrate repository working
- * <li>Added unit tests to demonstrate service working
+ * <li>Added unit tests to demonstrate repositories working
+ * <li>Added unit tests to demonstrate services working
+ * <li>Added unit tests to demonstrate controllers working
  * <li>Added logging in Both Discussion Thread and Post Service Classes
  * </ul>
  * 
