e7867d4c5b45c585e27e564ed57d9e404efc6fb9
John Richmond <jrichmond@enova.com>
2024-11-08 21:39:11 -0600

    updating package-info file to reflect all changes

--- src/main/java/edu/depaul/cdm/se452/d2l_mock/discussion_thread/package-info.java
+++ src/main/java/edu/depaul/cdm/se452/d2l_mock/discussion_thread/package-info.java
@@ -10,13 +10,19 @@
  * thread
  * <ul>
  * <li>UI for listing Discussion Threads and creating new Discussion
- * Threads
+ * Threads. Ken noted via email that this can be used as Non-Persistence not
+ * discussed in class.
  * <li>UI for showing a single Discussion Thread and adding a Post to a
- * Discussion Thread
+ * Discussion Thread. Ken noted via email that this can be used as
+ * Non-Persistence not discussed in class.
+ * <li>Added Repositories for Discussion Thread and Post classes
  * <li>Added unit tests to demonstrate repositories working
  * <li>Added unit tests to demonstrate services working
  * <li>Added unit tests to demonstrate controllers working
  * <li>Added logging in Both Discussion Thread and Post Service Classes
+ * <li>Added additional finder for PostRepository to find by StudentId
+ * <li>Added additional finder for DiscussionThreadRepository to find by
+ * StudentId
  * </ul>
  * 
  * @author John Richmond
