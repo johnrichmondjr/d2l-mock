76f3550f2c60fb1a4cc88ced0177b79ddd8bf9e5
Puneethnaidum <Harshavardhan@12345>
2024-11-13 12:59:29 -0500

    Added Loggers, testcase and login page as per final milestone

--- src/main/java/edu/depaul/cdm/se452/d2l_mock/user/UserRepository.java
+++ src/main/java/edu/depaul/cdm/se452/d2l_mock/user/UserRepository.java
@@ -1,9 +1,7 @@
 package edu.depaul.cdm.se452.d2l_mock.user;
 
 import java.util.Optional;
-
 import org.springframework.data.repository.CrudRepository;
-
 
 public interface UserRepository extends CrudRepository<AppUser, Integer> {
     Optional<AppUser> findByUsername(String username);
